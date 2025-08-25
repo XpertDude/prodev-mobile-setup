823df99d0bc0296d18adcd40b0cc9aba40372988
XpertDude <braodby@gmail.com>
2025-08-25 18:44:11 +0100

    Styling in React Native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -25,17 +25,16 @@ const styles = StyleSheet.create({
         textAlign: "center",
     },
 });
+
 export default function Index() {
     return (
-        <>
-            <View style={styles.container}>
-                <Text>Entry Screen - Awesome</Text>
-            </View>
+        <View style={styles.container}>
+            <Text>Entry Screen - Awesome</Text>
             <View>
                 <Text style={styles.largeText}>Typescript is great if you practice more</Text>
                 <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
                 <Text style={styles.smallText}>ALX is awesome</Text>
             </View>
-        </>
+        </View>
     );
 }
