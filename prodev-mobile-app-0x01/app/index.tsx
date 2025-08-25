c71410fa70d8b5ccf7b3417049c88a9957747967
XpertDude <braodby@gmail.com>
2025-08-25 22:09:43 +0100

    prodev-mobile-app-0x01

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -28,11 +28,13 @@ const styles = StyleSheet.create({
 
 export default function Index() {
     return (
-        <View style={styles.container}>
-            <Text>Entry Screen - Awesome</Text>
-            <Text style={styles.largeText}>Typescript is great if you practice more</Text>  
-            <Text style={styles.mediumText}>React Native provides you a single codebase for
-            cross platforms</Text>  <Text style={styles.smallText}>ALX is awesome</Text>
-        </View>
+<View style={styles.container}>
+    <Text>Entry Screen - Awesome</Text>
+    <View>
+  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
+    </View>
+</View>
     );
 }
