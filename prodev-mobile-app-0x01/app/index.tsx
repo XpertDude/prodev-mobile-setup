653ca315f3f5e350c8abf693be949cce41e9328a
XpertDude <braodby@gmail.com>
2025-08-25 18:29:47 +0100

    Styling in React Native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -31,12 +31,8 @@ export default function Index() {
         <SafeAreaView>
             <View style={styles.container}>
                 <Text>Entry Screen - Awesome</Text>
-                <Text style={styles.largeText}>
-                    Typescript is great if you practice more
-                </Text>
-                <Text style={styles.mediumText}>
-                    React Native provides you a single codebase for cross platforms
-                </Text>
+                <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+                <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
                 <Text style={styles.smallText}>ALX is awesome</Text>
             </View>
         </SafeAreaView>
